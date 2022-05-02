de596c2e32d463c3c59ae6a830aef2d8730ae22f
otsergeevicha <61205085+otsergeevicha@users.noreply.github.com>
2022-05-02 15:08:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float heroHealth = 100.5;
+            int heroArmor = 50;
+            int heroDamage = 14;
+            float speedHero;
+            string namePlayer;
+            string nameCastle;
+            int myAge;
+            char myNumberHouse = '1';
+            bool iDriver true;
+            bool iDancer;
+
+        }
+    }
+}
